9a4fc135e2ec5a68f0d8dc0a5149c6ae054c3d90
Aaron B. Sanchez <abs@sam-system.com>
2023-08-17 17:26:41 +0200

    trying to finish about page

---
+++ Cultris II/Cultris II/Views/CustomViews/TableRow2.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Cultris_II.Views.CustomViews
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+
+    public partial class TableRow2 : ContentView
+    {
+        public TableRow2()
+        {
+            InitializeComponent();
+        }
+    }
+}
